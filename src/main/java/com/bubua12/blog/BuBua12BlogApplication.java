588725748c29ab5f04becfa3bb8c5d4f5d562f25
bubua12 <wangty334752@gmail.com>
2024-05-28 23:04:54 +0800

    feat: Initial commit

---
+++ src/main/java/com/bubua12/blog/BuBua12BlogApplication.java
@@ -0,0 +1,17 @@
+package com.bubua12.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ * @author wangxl
+ * @since 2024/5/28 23:01
+ */
+@SpringBootApplication
+public class BuBua12BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BuBua12BlogApplication.class, args);
+    }
+}
